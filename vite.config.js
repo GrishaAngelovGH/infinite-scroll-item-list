43b29ae056576c348a99d0f441e460ddbbcbeb21
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-23 09:58:05 +0200

    Change the extension for components to jsx

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      selectors: "/src/selectors",
+      slices: "/src/slices",
+      images: "/src/assets/images",
+      icons: "/src/assets/icons"
+    }
+  },
 })
